63e9c6213a2d923844f8d687e8375f6db9591657
JimHalpert26 <rahulpadhy.15.09.1996@gmail.com>
2022-11-24 11:05:14 +0530

    Tried a few things

--- concurrentMergeSort.cpp
+++ concurrentMergeSort.cpp
@@ -1,3 +1,5 @@
+//#include <boost/random.hpp>
+//#include <boost/nondet_random.hpp>
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
@@ -10,114 +12,151 @@
 #include <unistd.h>
 using namespace std;
 
-int* inputArray;
-int* array1;
-int* array2;
+
 const int selectionSortLimit = 5;
 
-void selectionSort(int* inputArray, int low, int high) {
+
+// Selection Sorting when the parition size in mergeSort operation < 5
+void selectionSort(int array[], int low, int high) {
     int minimumIndex, i, j;
     for (i = low; i <= high; i++) {
         minimumIndex = i;
         for (j = i + 1; j <= high; j++) {
-            if (inputArray[minimumIndex] > inputArray[j])
+            if (array[j] < array[minimumIndex])
                 minimumIndex = j;
         }
-        swap(inputArray[minimumIndex], inputArray[i]);
+        swap(array[minimumIndex], array[i]);
     }
 }
 
-void merge(int* shmArray, int low, int mid, int high) {
-    int num1 = mid - low + 1, num2 = high - mid, i, j, k;
-    for (i = 0; i < num1; i++)
-        array1[i] = shmArray[low + i];
-    for (j = 0; j < num2; j++)
-        array2[j] = shmArray[mid + j + 1];
-    i = 0;
-    j = 0;
-    k = low;
+
+// Merge operation in Merge-Sorting
+void merge(int array[], int low, int mid, int high) {
+    int i = 0, j = 0, k = 0, num1 = mid - low + 1, num2 = high - mid;
+    int tempArray[high - low + 1];
     while (i < num1 && j < num2) {
-        if (array1[i] < array2[j])
-            shmArray[k++] = array1[i++];
+        if (array[i] < array[j])
+            tempArray[k++] = array[i++];
         else
-            shmArray[k++] = array2[j++];
+            tempArray[k++] = array[j++];
     }
     while (i < num1)
-        shmArray[k++] = array1[i++];
+        tempArray[k++] = array[i++];
     while (j < num2)
-        shmArray[k++] = array2[j++];
+        tempArray[k++] = array[j++];
+    for (i = low; i <= high; i++)
+        array[i] = tempArray[i - low];
 }
 
-void mergeSort(int* shmArray, int low, int high) {
+
+// Merge-Sorting
+void mergeSort(int array[], int low, int high) {
+    
+    // Default case
     if (low >= high)
         return;
     if (high - low + 1 < selectionSortLimit) {
-        selectionSort(shmArray, low, high);
+        selectionSort(array, low, high);
         return;
     }
-    int mid = low + (high - low) / 2, leftChild, rightChild, status;
+
+    // calling fork upon the left and right parts of the array
+    int mid = low + (high - low) / 2, leftChild, rightChild, status = 0;
     leftChild = fork();
     if (leftChild < 0) {
         cout << "Error in Fork for leftChild!!" << endl;
-        exit(1);
+        exit(-1);
     }
     else if (!leftChild) {
-        mergeSort(shmArray, low, mid);
+        mergeSort(array, low, mid);
         exit(0);
     }
     else {
         rightChild = fork();
         if (rightChild < 0) {
             cout << "Error in Fork for rightChild!!" << endl;
-            exit(1);
+            exit(-1);
         }
         else if (!rightChild) {
-            mergeSort(shmArray, mid + 1, high);
+            mergeSort(array, mid + 1, high);
             exit(0);
         }
     }
+
+    // waiting for the children processes to finish
     waitpid(leftChild, &status, 0);
     waitpid(rightChild, &status, 0);
-    merge(shmArray, low, mid, high);
+
+    // merging the sorted left and right partitions
+    merge(array, low, mid, high);
 }
 
+
 int main() {
+
+    // Generic variable declaration and starting the clock
     time_t startNow, endNow;
     time(&startNow);
-    int i, arraySize, minValue = 1, maxValue = 1e6, id, matID;
+    int i, arraySize, minValue = 1, maxValue = 1e6, shmID;
+    key_t k = IPC_PRIVATE;
     int* shmArray;
-    key_t k;
     cin >> arraySize;
-    inputArray = (int*)malloc(sizeof(int) * (arraySize + 1));
-    array1 = (int*)malloc(sizeof(int) * (arraySize + 1));
-    array2 = (int*)malloc(sizeof(int) * (arraySize + 1));
-    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-    default_random_engine generator(seed);
-    uniform_int_distribution<int> distribution(minValue, maxValue);
-    for (i = 0; i < arraySize; i++)
-        inputArray[i] = distribution(generator);
-    k = IPC_PRIVATE;
-    id = shmget(k, arraySize * sizeof(int), IPC_CREAT | 0666);
-    if (id < 0) {
-        cout << "Error in doing shmget!!" << endl;
+
+    // Calculating segment length and creating the segment
+    size_t shmSize = sizeof(int) * arraySize;
+    if ((shmID = shmget(k, shmSize, IPC_CREAT | 0666)) < 0) {
+        cout << "Error while doing shmget!!" << endl;
         exit(1);
     }
-    shmArray = (int*)shmat(id, NULL, 0);
-    if (matID < 0) {
-        cout << "Error in doing shmat!!" << endl;
+
+    // Attaching the segment to the data space
+    if ((shmArray = (int*)shmat(shmID, NULL, 0)) == (int*)-1) {
+        cout << "Error while doing shmat!!" << endl;
         exit(1);
     }
+
+    // Assigning random numbers to the input array
+    
+    //unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    //default_random_engine generator(seed);
+    //uniform_int_distribution<int> uniformDistribution(minValue, maxValue);
+    
+    //boost::random::random_device randomDevice;
+    //boost::random::mt19937 generator(randomDevice());
+    //boost::random::uniform_int_distribution<> uniformDistribution(minValue, maxValue);
+
+    random_device randomDevice;
+    mt19937 generator(randomDevice());
+    uniform_int_distribution<int> uniformDistribution(minValue, maxValue);
     for (i = 0; i < arraySize; i++)
-        shmArray[i] = inputArray[i];
+        shmArray[i] = uniformDistribution(generator);
+    
+    // Crux of the question
     mergeSort(shmArray, 0, arraySize - 1);
+    
+    // Verification that the sorting actually happened
+    for (i = 1; i < arraySize; i++) {
+        cout << shmArray[i] << ' ';
+        if (shmArray[i] < shmArray[i - 1]) {
+            cout << "Still not sorted!!" << endl;
+            exit(1);
+        }
+    }
+    cout << endl;
+
+    // Detatching from the shared memory
     if (shmdt(shmArray) == -1) {
         cout << "Error in shmdt!!" << endl;
         exit(1);
     }
-    if (shmctl(id, IPC_RMID, NULL) == -1) {
-        cout << "Error in shmcelt!!" << endl;
+
+    // Deleting the shared memory
+    if (shmctl(shmID, IPC_RMID, NULL) == -1) {
+        cout << "Error in shmctl!!" << endl;
         exit(1);
     }
+    
+    // Calculating the time taken till mergesort completion
     time(&endNow);
     double timeTaken = double(endNow - startNow);
     cout << "Time taken : " << fixed << timeTaken << setprecision(3) << " seconds." << endl;
